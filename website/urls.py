8ca2ccaaf64b22fde9b106ec780035facb7e1290
Dilan Shanuka <99383107+IT21182914@users.noreply.github.com>
2024-07-27 01:33:29 +0530

    Login Function

--- website/urls.py
+++ website/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     # path('login/', views.login_user, name='login'),
-    # path('logout/', views.logout_user, name='logout'),
+     path('logout/', views.logout_user, name='logout'),
 
 ]
--- website/views.py
+++ website/views.py
@@ -1,11 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages as message
 
 def home(request):
-    return render(request, 'home.html')
+    # Check to see if logged in
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
 
-def login_user(request):
-    pass
+        if user is not None:
+            login(request, user)
+            message.success(request, 'You have successfully logged in')
+            return redirect('home')
+        else:
+            message.success(request, 'Error logging in. Please try again')
+            return redirect('home')
+
+    else:
+        return render(request, 'home.html',{})
+
+
 
 def logout_user(request):
-    pass
+
+    logout(request)
+    message.success(request, 'You have successfully logged out')
+    return redirect('home')
