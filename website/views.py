888b703d184e794f02f66c86bca62a4d69494a32
Dilan Shanuka <99383107+IT21182914@users.noreply.github.com>
2024-07-26 22:36:56 +0530

    Login Frontend

--- website/views.py
+++ website/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
-
+from django.contrib.auth import authenticate, login, logout
 
 def home(request):
     return render(request, 'home.html')
+
+def login_user(request):
+    pass
+
+def logout_user(request):
+    pass
