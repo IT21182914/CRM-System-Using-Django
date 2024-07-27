ff1f2bd7b630d60381300999eb5173453b2516b8
Dilan Shanuka <99383107+IT21182914@users.noreply.github.com>
2024-07-27 17:50:13 +0530

    Fetched All

--- website/views.py
+++ website/views.py
@@ -2,10 +2,9 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .forms import SignUpForm
-
+from .models import Record
 
 def home(request):
-    # Check to see if logged in
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
@@ -19,14 +18,13 @@ def home(request):
             messages.error(request, 'Error logging in. Please try again')
             return redirect('home')
     else:
-        return render(request, 'home.html', {})
-
+        records = Record.objects.all()  # Get all records
+        return render(request, 'home.html', {'records': records})
 
 def logout_user(request):
     logout(request)
     messages.success(request, 'You have successfully logged out')
     return redirect('home')
-
 
 def register_user(request):
     if request.method == 'POST':
